bed1b9c204776f7015f9150e1fb2f30cff24dbbc
Balaj Maqbool <balajmaqbool54@gmail.com>
2025-06-20 14:29:49 +0500

    adding hamburger menu

--- JS/script.js
+++ JS/script.js
@@ -59,21 +59,21 @@ const renderProjects = () => {
 }
 
 
-// const mobileMenu = document.getElementById('mobile-menu');
-// const navLinks = document.getElementById('nav-links');
+const mobileMenu = document.getElementById('mobile-menu');
+const navLinks = document.getElementById('nav-links');
 
-// mobileMenu.addEventListener('click', () => {
-//     navLinks.classList.toggle('active');
-//     mobileMenu.innerHTML = navLinks.classList.contains('active') ?
-//         '<i class="fas fa-times"></i>' : '<i class="fas fa-bars"></i>';
-// });
+mobileMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    mobileMenu.innerHTML = navLinks.classList.contains('active') ?
+        '<i class="fas fa-times"></i>' : '<i class="fas fa-bars"></i>';
+});
 
-// document.querySelectorAll('.nav-links a').forEach(link => {
-//     link.addEventListener('click', () => {
-//         navLinks.classList.remove('active');
-//         mobileMenu.innerHTML = '<i class="fas fa-bars"></i>';
-//     });
-// });
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+        mobileMenu.innerHTML = '<i class="fas fa-bars"></i>';
+    });
+});
 
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
